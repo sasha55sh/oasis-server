4b94d6740fb64eaf7f21165129dcc385e2d8c4c2
sasha55sh <150395476+sasha55sh@users.noreply.github.com>
2025-05-29 19:57:06 +0300

    update routes

--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
+const menuController = require("../controllers/MenuController");
+
 router.get("/", (req, res) => {
   res.send("Виберіть маршрут");
 });
+
+router.get("/menu", menuController.getMenu);
 
 module.exports = router;
