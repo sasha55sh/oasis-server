1a82c726ff5980b7741d33967c946ebe5773c080
sasha55sh <150395476+sasha55sh@users.noreply.github.com>
2025-06-30 17:36:54 +0300

    features: create models for faq, menu and product

---
+++ models/menuModel.js
@@ -0,0 +1,12 @@
+const { Schema, model } = require("mongoose");
+
+const menuSchema = new Schema({
+  id: { type: String },
+  title: { type: String },
+  description: { type: String },
+  cal: { type: Number },
+  category: { type: String , required: true},
+  price: { type: Number },
+});
+
+module.exports = model("Menu", menuSchema, "menu");
