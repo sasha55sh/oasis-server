2859c2c0905b1d1c728047e35ad1c3bbf8abc577
sasha55sh <150395476+sasha55sh@users.noreply.github.com>
2025-06-30 17:39:31 +0300

    features: create controller for faq, menu and product

---
+++ controllers/MenuController.js
@@ -0,0 +1,11 @@
+const MenuService = require("../services/MenuService");
+
+class MenuController {
+  async getMenu(req, res) {
+    const { category } = req.query;
+    const menu = await MenuService.getMenuByCategory(category);
+    return res.status(200).json(menu);
+  }
+}
+
+module.exports = new MenuController();
