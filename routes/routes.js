c637a320f359ec2ec1d73707c39e1fe540e57c7c
sasha55sh <150395476+sasha55sh@users.noreply.github.com>
2025-06-30 17:40:22 +0300

    update: add new routes

--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,28 @@
 const express = require("express");
 const router = express.Router();
 
+<<<<<<< Updated upstream
+=======
+const MenuController = require("../controllers/MenuController");
+const FaqController = require("../controllers/FaqController");
+const ProductController = require("../controllers/ProductController");
+const ProductService = require("../services/ProductService");
+
+>>>>>>> Stashed changes
 router.get("/", (req, res) => {
   res.send("Виберіть маршрут");
 });
 
+<<<<<<< Updated upstream
+=======
+router.get("/menu", MenuController.getMenu);
+router.get("/faq", FaqController.getFaq);
+
+router.get("/shop", ProductController.getAllProducts);
+router.get("/shop", ProductService.getProductsByCategory);
+router.get("/shop", ProductController.getFilteredProducts);
+router.get("/shop", ProductController.getProductByTitle);
+router.get("/shop/:handle", ProductController.getProductByHandle);
+
+>>>>>>> Stashed changes
 module.exports = router;
