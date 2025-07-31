381b41ddf5382b0e0be9c54c457823fc5ebec705
sasha55sh <150395476+sasha55sh@users.noreply.github.com>
2025-07-31 14:28:32 +0300

    Create AuthService.js

---
+++ services/AuthService.js
@@ -0,0 +1,28 @@
+const admin = require("../config/firebase");
+const User = require("../models/userModel");
+const jwt = require("jsonwebtoken");
+
+class AuthService {
+  async verifyCodeService(idToken) {
+    const decoded = await admin.auth().verifyIdToken(idToken);
+    const { uid, phone_number } = decoded;
+
+    let user = await User.findOne({ phone: phone_number });
+    if (user) {
+      user.uid = uid;
+      await user.save();
+    } else {
+      user = await User.create({
+        uid,
+        phone: phone_number,
+      });
+    }
+
+    const token = jwt.sign({ uid: user.uid }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+    return { user, token };
+  }
+}
+
+module.exports = new AuthService();
