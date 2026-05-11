28981a7c18aa44f0a6aecdfd8b34c60ed6ffe2f3
sasha55sh <150395476+sasha55sh@users.noreply.github.com>
2026-05-11 16:04:30 +0300

    Small fix

--- models/nutritionRequest.js
+++ models/nutritionRequest.js
@@ -2,20 +2,24 @@ const { Schema, model } = require("mongoose")
 
 const nutritionProgramRequestSchema = new Schema({
     userData: {
-        uid: { type: String},
+        uid: { type: String },
         firstName: { type: String },
         phoneNumber: { type: String },
+        method: {
+            type: String,
+            enum: ["self-pickup", "courier"],
+        },
         street: { type: String },
         flat: { type: String },
         house: { type: String },
-        comments: { type: String },
     },
     programData: {
         title: { type: String },
         kcal: { type: Number },
         duration: { type: Number },
         totalPrice: { type: Number }
-    }
+    },
+    comments: { type: String },
 })
 
 module.exports = model("NutritionRequest", nutritionProgramRequestSchema, "nutrition-request")
